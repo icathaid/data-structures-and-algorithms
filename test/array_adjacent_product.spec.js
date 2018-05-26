4509a4c468d5639e9078fbd11c215eeacb6bcc6a
James McDaniel <icathaid@gmail.com>
2018-05-26 15:12:19 -0700

    fixed github issue

--- test/array_adjacent_product.spec.js
+++ test/array_adjacent_product.spec.js
@@ -1,9 +1,9 @@
 'use strict';
+
 const matrix = require('../lib/array_adjacent_product.js');
 
 describe('test function', () => {
-  it('should return undefined if given testarrayIncludesUndefined and undefined', () => {
-    const actual = matrix();
-    expect(actual).toEqual(0);
+  it('should return 0', () => {
+    expect(matrix()).toEqual(0);
   });
 });
