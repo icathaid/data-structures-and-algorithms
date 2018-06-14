c4c0f08a94063b8e531e9876a20da0aba65fe21a
James McDaniel <icathaid@gmail.com>
2018-06-13 20:08:08 -0700

    added pseudocode for remaining methods

--- test/ll_insertions.spec.js
+++ test/ll_insertions.spec.js
@@ -7,6 +7,8 @@ describe('Linked List Module', () => {
     expect(bill.next).toBeUndefined();
     expect(bill.length).toEqual(0);
   });
+});
+describe('Linked List Append Method', () => {
   it('Should return head:1, next: null when called with 1', () => {
     let bill = new NewList(1);
     expect(bill.head.data).toEqual(1);
@@ -20,9 +22,13 @@ describe('Linked List Module', () => {
   });
   it('Should return 3 when appended again', () => {
     let bill = new NewList(1);
-    bill.append(2)
+    bill.append(2);
     bill.append(3);
+    expect(bill.head.next.data).toEqual(2);
     expect(bill.head.next.next.data).toEqual(3);
     expect(bill.head.next.next.next).toBeNull();
   });
+});
+describe('Linked List InsertBefore Method', () => {
+  
 });
